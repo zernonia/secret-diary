3e2774c463c793c440a48e1da9890760d9eb8c86
zernonia <59365435+zernonia@users.noreply.github.com>
2021-08-04 01:40:12 +0800

    🐛 Fix Windi prose not working

--- src/pages/diary.vue
+++ src/pages/diary.vue
@@ -1,15 +1,11 @@
 <template>
-  <div class="diary p-8 flex flex-col w-screen-md h-screen-md border-3 border-dark-900 rounded-xl">
+  <div>
     <h1>Secret Diary</h1>
-    <div class="w-full h-full">
-      <router-view></router-view>
+    <div class="diary p-8 relative flex flex-col w-screen-md h-screen-md border-3 border-dark-900 rounded-xl">
+      <div class="w-full h-full">
+        <router-view></router-view>
+      </div>
+      <button class="">Write diary</button>
     </div>
   </div>
 </template>
-
-<style>
-.diary-page {
-  background-color: white;
-  background-image: url("data:image/svg+xml,%3Csvg width='20' height='20' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%23beaed9' fill-opacity='0.11' fill-rule='evenodd'%3E%3Ccircle cx='0' cy='0' r='0'/%3E%3Ccircle cx='15' cy='15' r='3'/%3E%3C/g%3E%3C/svg%3E");
-}
-</style>
